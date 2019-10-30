61162671befca67dd3a77087820fd1e99a0277d6
David Larpent <david.larpent@me.com>
2019-10-30 18:05:03 +0000

    Add navbar brand

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,7 +41,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-                <img src="{logo}" alt="pha logo" />
+                <img src={logo} alt="pha logo" />
             </Link>
             {/* Hamburger menu */}
             <div
