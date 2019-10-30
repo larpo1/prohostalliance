81214548eb1f6e6167fe53250bc4247eb128b0f1
David Larpent <david.larpent@me.com>
2019-10-30 19:58:18 +0000

    Tidy up

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -38,7 +38,7 @@ export const IndexPageTemplate = ({
       <h2 className="subtitle has-text-light">
         We are launching soon. In the mean time, please...
       </h2>
-      <Link to="/register" className="button is-primary">Join us</Link>
+      <Link to="/register" className="button is-primary">JOIN US</Link>
     </div>
   </div>
  </section>
